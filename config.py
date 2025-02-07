7e86ff314735a70eb8887e5da0dc8967bed2e1ce
schytze0 <schytze@gmx.de>
2025-02-07 11:09:49 +0000

    Added models folder and modified config file to check if folder exists

--- config.py
+++ config.py
@@ -8,12 +8,19 @@ KAGGLE_KEY = os.getenv("KAGGLE_KEY", "")
 ROOT_FOLDER = "/root/.cache/kagglehub/datasets/vipoooool/new-plant-diseases-dataset/versions/2"
 DATA_DIR = os.path.join(ROOT_FOLDER, "new plant diseases dataset(augmented)/New Plant Diseases Dataset(Augmented)/")
 
-# Define separate paths for training and validation
+# Define paths for training and validation
 TRAIN_PATH = os.path.join(DATA_DIR, "train")
 VALID_PATH = os.path.join(DATA_DIR, "valid")
+
+# Model save path
+MODEL_DIR = "models"  # Directory to store trained models
+MODEL_PATH = os.path.join(MODEL_DIR, "plant_disease_model.h5")  # Path for saving/loading the model
 
 # Training parameters
 BATCH_SIZE = 32
 IMG_SIZE = (224, 224)
 EPOCHS = 20
 
+# Ensure the model directory exists
+if not os.path.exists(MODEL_DIR):
+    os.makedirs(MODEL_DIR)
