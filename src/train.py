9507b3f592df92e5bd2b5b2417f15d236ccd6a67
Philipp Kleer <philipp.kleer@posteo.com>
2025-02-19 11:06:01 -0300

    transfered functions to helpers.py

--- src/train.py
+++ src/train.py
@@ -5,39 +5,7 @@ from data_loader import load_data
 from model import build_vgg16_model
 from config import MODEL_PATH, HISTORY_PATH, EPOCHS, BATCH_SIZE, PROC_DIR, NUM_CLASSES
 import os
-
-# including saved train_/val_data.tfrecord
-def _parse_function(proto):
-    # Define the feature structure
-    keys_to_features = {
-        'image': tf.io.FixedLenFeature([], tf.string),
-        'label': tf.io.FixedLenFeature([], tf.int64),
-    }
-
-    # Parse the input `tf.train.Example` proto using the feature structure
-    parsed_features = tf.io.parse_single_example(proto, keys_to_features)
-
-    # Decode the image data
-    image = tf.io.decode_jpeg(parsed_features['image'], channels=3)
-
-    # One-hot encode the label (assuming 38 classes)
-    label = tf.one_hot(parsed_features['label'], depth=NUM_CLASSES)
-
-    return image, label
-
-def load_tfrecord_data(tfrecord_file):
-    # Create a dataset from the TFRecord file
-    dataset = tf.data.TFRecordDataset(tfrecord_file)
-
-    # Map the parsing function over the dataset
-    dataset = dataset.map(_parse_function)
-
-    # Shuffle, batch, and prefetch the dataset for performance
-    dataset = dataset.shuffle(buffer_size=10000)
-    dataset = dataset.batch(BATCH_SIZE)
-    dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
-
-    return dataset
+from helpers import load_tfrecord_data
 
 
 # Old function adjusted
