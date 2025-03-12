5345d17f53045ff03a87b5e3493cfd0a94aeea74
philkleer <philipp.kleer@posteo.com>
2025-03-12 08:31:02 -0300

    - modified API /train: deleted data path (now implemented via data_loader.py); added try/except

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, HTTPException
 from pydantic import BaseModel
 from src.train import train_model  # Import train_model function from train.py
 from src.predict import predict_single_image  # Import predict_single_image function from predict.py
@@ -14,8 +14,14 @@ async def root():
 
 @app.post("/train")
 async def train_model_endpoint(request: TrainRequest):
-    train_model(request.dataset_path)
-    return {"message": "Model training started."}
+    # train_model(request.dataset_path)
+    # REVIEW: the model is not given with the api, the data fetch is implemented in src/data_loader.py and called in train.py
+    # REVIEW: I added try/except to get messages if training successfully ended and error if not
+    try:
+        train_model()
+        return{"message": "Model successfully trained."}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f'Training failed: {str(e)}')
 
 # Define /predict endpoint that uses predict_single_image
 @app.post("/predict/")
