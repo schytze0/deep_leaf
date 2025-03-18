204f89807adc36605b8974e972904e9c7043be67
philkleer <philipp.kleer@posteo.com>
2025-03-18 20:01:56 -0300

    working now

--- src/predict.py
+++ src/predict.py
@@ -8,7 +8,7 @@ from io import BytesIO
 import argparse
 
 # user defined functions and libraries
-from src.config import MODEL_PATH, IMG_SIZE, TEST_PATH
+from src.config import MODEL_PATH, IMG_SIZE
 from src.helpers import load_tfrecord_data
 
 # user defined class for f1 score
@@ -35,14 +35,14 @@ class F1Score(tf.keras.metrics.Metric):
 
 def load_trained_model():
     '''
-    Loads the trained model from disk.
+    Loads the trained model from disk with custom object class F1Score
     
     Returns:
     - model: The loaded Keras model.
     '''
     return tf.keras.models.load_model(
         MODEL_PATH,
-        custom_objects={'F1Score': F1Score}  # `custom_objects` instead of `custom_object`
+        custom_objects={'F1Score': F1Score}
     )
 
 def get_class_labels():
@@ -50,26 +50,22 @@ def get_class_labels():
     Retrieves class labels dynamically from the training data.
 
     Returns:
-    - class_labels (dict): Mapping from index to class label.
+    - class_labels (set): Mapping from index to class label.
     '''
-
+    # DISCUSS: Maybe doing this manually is more efficient? Because now we do this everytime, which seems unnecessary
     train_data = load_tfrecord_data('data/training/train.tfrecord')
 
-    # Print the structure of train_data to understand its type and contents
+    # get the data of the loaded train_data
     dataset = train_data[0]
 
-    # class_labels = []
     class_labels = set()
+
     # decoding one-hot back to class indices
-    for images, labels in dataset.take(1):
-        # class_indices = np.argmax(labels, axis=-1)
-        # class_labels.update(class_indices)
-        # new approach
-        class_indices = np.argmax(labels, axis=-1)  # Assuming the labels are one-hot encoded
+    for images, labels in dataset:
+        class_indices = np.argmax(labels, axis=-1)  
         unique_labels = np.unique(class_indices)
         class_labels.update(unique_labels)
 
-    print(f'CLass labels: {class_labels}')
     return sorted(class_labels)
 
 def preprocess_image(file): # nvd06: changed from image_path to file to fit the main.yp and FastAPI setup. 
@@ -82,14 +78,16 @@ def preprocess_image(file): # nvd06: changed from image_path to file to fit the 
     Returns:
     - img_array: Preprocessed image array.
     '''
-    if isinstance(file, str):  # If it's a file path (for CLI)
+    # local path transfer or via FastAPI
+    if isinstance(file, str):  
+        # via CLI giving path
         img = image.load_img(file, target_size=IMG_SIZE)
-    else:  # If it's a file-like object (for FastAPI)
+    else: 
+        # via FastApi transfer file
         img = image.load_img(BytesIO(file.file.read()), target_size=IMG_SIZE)
 
-    # img = image.load_img(BytesIO(file.file.read()), target_size=IMG_SIZE) # added the BytesIO part for file handling nvd06
-    img_array = image.img_to_array(img) / 255.0  # Normalize pixel values
-    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
+    img_array = image.img_to_array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
     return img_array
 
 def preprocess_image_from_path(image_path):
@@ -108,16 +106,17 @@ def predict_single_image(file):
     Returns:
     - Predicted class label.
     '''
+    
+    # load model
     model = load_trained_model()
     
-    class_labels = get_class_labels()  # Get dynamic class labels
+    # get classes
+    class_labels = get_class_labels() 
     
+    # preprocessing
     img_array = preprocess_image(file) 
     predictions = model.predict(img_array)
     class_index = np.argmax(predictions, axis=1)[0]
-
-    print(f"Class index: {class_index}")
-    print(f"Class labels: {class_labels}")
 
     return class_labels[class_index]  # Return class label instead of index
 
@@ -132,5 +131,3 @@ if __name__ == '__main__':
     results = predict_single_image(args.image_path)
 
     print(f'\nPredictions for Test Set: {results}')
-    # for filename, label in results.items():
-    #     print(f'{filename}: {label}')
